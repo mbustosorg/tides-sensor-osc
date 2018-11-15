dbecb4d6bf4b2052100712616c65bd071fcb784d
Mauricio Bustos <m@bustos.org>
2018-11-15 07:10:51 +0000

    minor script update

--- startup_script.sh
+++ startup_script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 process=tides_sensor_osc
-makerun="/home/pi/tides_sensor_osc/bin/tides_sensor_osc"
+makerun="/home/pi/tides-sensor-osc/bin/tides_sensor_osc"
 
 if ps ax | grep -v grep | grep $process > /dev/null
 then
