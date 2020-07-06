4c8b8239b77175afc4fff769011a2a42ff8b2cf9
Mauricio Bustos <m@bustos.org>
2020-07-06 05:43:34 +0000

    add testing

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="Mauricio Bustos",
+    version="0.1",
+    author="Mauricio Bustos",
+    author_email="m@bustos.org",
+    description="Level controller for Changing Tides",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
